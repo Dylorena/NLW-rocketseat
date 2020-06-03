1038341267f7e23f9b9c06b6c851e3dc9497b1bf
dylorena <dilorenasantos@gmail.com>
2020-06-02 23:51:56 -0300

    Adiciona parte do cadastro referente a dados da entidade

---
+++ scripts/create-point.js
@@ -0,0 +1,44 @@
+function populateufs(){
+    const ufselect = document.querySelector("select[name=uf]")
+    
+    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then(res => res.json()
+    ).then ( states => {
+        for(state of states){
+            ufselect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+        }     
+        
+    } )
+}
+
+//`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${state.sigla}/municipios`
+
+function getCities(event){
+    const cityselect = document.querySelector("select[name=city]")
+    const stateInput = document.querySelector("input[name=state]")
+
+    const ufValue = event.target.value
+
+    const index = event.target.value
+    stateInput.value = event.target.options[index]
+
+    fetch(`https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`)
+    .then(res => res.json()
+    ).then ( cities => {
+        for(city of cities){
+            cityselect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+        }
+
+        cityselect.disabled = false
+    } )
+}
+
+populateufs();
+
+
+
+document.querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+        console.log("mudei")
+
+
