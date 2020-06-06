ad58c0f587572cea74beffd35ab1d654c6bc9a7a
dylorena <dilorenasantos@gmail.com>
2020-06-06 15:01:58 -0300

    Adiciona o menu das páginas secundárias em um único arquivo

--- src/server.js
+++ src/server.js
@@ -4,15 +4,27 @@ const server = express() // será um obejto de servidor
 //configurar a pasta publica
 server.use(express.static("public"))
 
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views",{
+    express: server,
+    noCache: true
+})
+
 //configurar caminho da aplicação
 //Página inicial
 //requisição e resposta
 server.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html",{
+        title: "Seu marketplace de coleta de resíduos."
+    })
 })
 
 server.get("/create-point", (req,res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search-results", (req,res) => {
+    return res.render("search-results.html")
 })
 
 // ligar o servidor
